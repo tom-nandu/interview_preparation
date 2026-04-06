1bfc6849dc5416fc5009680a1c1b4b2926524f50
tom-nandu <nandunkks@gmail.com>
2026-04-07 01:22:11 +0530

    Implement first occurrence binary search function

--- 18-22.c
+++ 18-22.c
@@ -40,4 +40,47 @@ int main()
 
     return 0;
 }
-``
+---------------------------------------------
+21. Find first occurrence using binary search
+    
+#include <stdio.h>
+
+int first_occurrence(int arr[], int l, int r, int v)
+{
+    int result = -1;
+
+    while (l <= r)
+    {
+        int m = l + (r - l) / 2;
+
+        if (arr[m] == v)
+        {
+            result = m;     // store index
+            r = m - 1;      // move left to find first occurrence
+        }
+        else if (arr[m] > v)
+        {
+            r = m - 1;
+        }
+        else
+        {
+            l = m + 1;
+        }
+    }
+    return result;
+}
+
+int main()
+{
+    int arr[] = {1, 2, 3, 3, 3, 5, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    int v = 3;
+    int index = first_occurrence(arr, 0, n - 1, v);
+
+    printf("First occurrence of %d is at index %d\n", v, index);
+    return 0;
+}
+
+------------------------------------------------
+22. Find last occurrence using binary search    
